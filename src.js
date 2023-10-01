a1b003565540c5479a04b158e9deb34cad7064b8
Abraham <enarebebenatthan@gmail.com>
2023-10-01 16:20:29 -0700

    FIRST COMIT

--- src.js
+++ src.js
@@ -1,3 +1,26 @@
-function loadRiddle(params) {
-    
+
+function loadRiddle(randomizedHash, prisonerNo) {
+    // The time complexity O(noOfBoxToOpen)
+
+    var noOfBoxToOpen = 50
+    var hashSize = 100
+    var positionOfPointeer = prisonerNo
+    var doNotKill = false
+
+    for (let i = 0; i <= noOfBoxToOpen; i++) {
+        if (randomizedHash[positionOfPointeer] == prisonerNo) {
+            doNotKill = true
+            i = noOfBoxToOpen+1
+        }
+        if (positionOfPointeer == hashSize) {
+            positionOfPointeer = 1
+        }
+        else{
+            positionOfPointeer++
+        }
+        
+    }
+
+    return doNotKill
+
 }
